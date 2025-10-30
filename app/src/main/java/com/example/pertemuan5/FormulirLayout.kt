7af5f016c0c651816d8d2d6b2b244c0442ebfa0b
Lalu Fatan <lalufatan25@gmail.com>
2025-10-30 15:22:25 +0700

    nambahin outline text filed

--- app/src/main/java/com/example/pertemuan5/FormulirLayout.kt
+++ app/src/main/java/com/example/pertemuan5/FormulirLayout.kt
@@ -1,15 +1,12 @@
 package com.example.pertemuan5
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,15 +15,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.unit.dp
-import com.example.pertemuan5.ui.theme.Pertemuan5Theme
 
 @Composable
 
 fun FormulirScreen(modifier: Modifier = Modifier) {
-    var inputNama by remember { mutableStateOf("") }
+    var textNama by remember { mutableStateOf("") }
     var inputAlamat by remember { mutableStateOf("") }
     var selectedGender by remember { mutableStateOf("") }
 
@@ -36,8 +31,19 @@ fun FormulirScreen(modifier: Modifier = Modifier) {
 
     val genderOptions = listOf("Laki-laki", "Perempuan")
 
-    Column(modifier = modifier.padding(yop = 50.dp),
+    Column(modifier = modifier.padding(top = 50.dp),
         verticalArrangement =  Arrangement.Top,
         horizontalAlignment =  Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = modifier.width(width = 250.dp),
+            label = { Text(text = "nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
 
-        }
+        )
+    }
